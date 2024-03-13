b288ab121614765a73d364a1425aabc0ab8762e0
sameerg1810 <64128604+sameerg1810@users.noreply.github.com>
2024-03-13 16:59:27 +0530

    removed depracated functions of mongodb

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import http from "http";
 import compression from "compression";
-import routes from "./routes/index.js";
 import { connect } from "mongoose";
 import dotenv from "dotenv";
 import { Server as SocketIOServer } from "socket.io";
@@ -23,11 +22,8 @@ app.use(cors(corsOptions)); // Apply CORS middleware with the options
 app.use(compression({ threshold: 2048 }));
 app.use(express.json()); // Ensure express.json() is used instead of json()
 
-// MongoDB connection
-connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+// MongoDB connection without deprecated options
+connect(process.env.MONGO_URI || "YOUR_MONGO_URI")
   .then(() => console.log("Connected to MongoDB"))
   .catch((error) => console.error("MongoDB connection error:", error));
 
@@ -56,8 +52,6 @@ io.on("connection", (socket) => {
     console.log("User disconnected");
   });
 });
-
-app.use("/", routes); // Assuming routes are defined in a separate routes.js file
 
 // Improved global error handler
 app.use((err, req, res, next) => {
