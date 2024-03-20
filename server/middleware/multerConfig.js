5a10a9e1e0a867837e5051523069a66df644064e
sameerg1810 <64128604+sameerg1810@users.noreply.github.com>
2024-03-20 12:47:58 +0530

    added middleware filesystem fs to create upload

--- server/middleware/multerConfig.js
+++ server/middleware/multerConfig.js
@@ -1,17 +1,29 @@
 import multer from "multer";
 import path from "path";
+import fs from "fs";
 
 // Define a function to sanitize filenames
 const sanitizeFilename = (filename) => {
   return filename.replace(/[^a-z0-9]/gi, "_");
 };
 
+// Function to ensure the uploads directory exists
+const ensureUploadsDirectory = () => {
+  const currentDir = path.resolve(path.dirname(""));
+  const uploadsPath = path.join(currentDir, "uploads");
+
+  if (!fs.existsSync(uploadsPath)) {
+    fs.mkdirSync(uploadsPath);
+  }
+};
+
+// Ensure the uploads directory exists
+ensureUploadsDirectory();
+
 // Set up Multer storage
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    // Define the destination folder dynamically using path.join()
-    const uploadDestination = path.join(__dirname, "../uploads/");
-    cb(null, uploadDestination);
+    cb(null, "uploads/");
   },
   filename: (req, file, cb) => {
     const sanitizedFilename = sanitizeFilename(file.originalname);
